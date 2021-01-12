0c0bd531d001fab5072d2ea7b3e297adc3de61c2
Rileaze <faizraxmanovelik@gmail.com>
2021-01-12 16:20:35 +0300

    Start again v2.0

---
+++ Game Rebuild/Game Rebuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game_Rebuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
