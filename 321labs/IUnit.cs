71168ed26c493f54afcefa2b76525fa2f5c03836
lemon <daniil2015473@gmail.com>
2021-01-12 16:38:49 +0300

    12.01.2021 interface ICommonUnit has been create

---
+++ 321labs/IUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs
+{
+    interface ICommonUnit
+    {
+        string Name { get; set; }
+        int Hp { get; set; }
+        int Strenght { get; set; }
+        bool IsMove { get; set; }
+        int Armor { get; set; }
+        double Evasion { get; set; }
+
+    }
+}
