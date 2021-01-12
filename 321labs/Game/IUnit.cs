431dd6cb8a2c04ee02facd699963eb5ed9400424
Rtakale <dialogbgr@mail.ru>
2021-01-12 15:42:31 +0300

    Add interface IUnit

---
+++ 321labs/Game/IUnit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _321labs.Game
+{
+    interface IUnit
+    {
+        public int Helth { get; set; }
+        public int Damage { get; set; }
+        public int Speed { get; set; }
+        public int Range { get; set; }
+    }
+}
