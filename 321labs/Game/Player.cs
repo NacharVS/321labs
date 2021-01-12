a78a058f20bdc84b07bb26ba992855d03f32d4e2
avocado <arhipovaanastasia16@gmail.com>
2021-01-12 16:47:27 +0300

    end 1 lesson 1 lab 12.01.2021

---
+++ 321labs/Game/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Game
+{
+    class Player
+    {
+        private string name;
+        private int power;
+        private int health;
+
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public int Health { get; set; }
+
+
+    }
+}
