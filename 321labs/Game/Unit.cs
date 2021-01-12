c37a4110cbad28cfe4ce52fe3a5048b83895436d
Diistaf <kikopoint@mail.ru>
2021-01-12 15:57:56 +0300

    class Unit modified

--- 321labs/Game/Unit.cs
+++ 321labs/Game/Unit.cs
@@ -6,5 +6,25 @@ namespace _321labs.Game
 {
     class Unit
     {
+        private int hp;
+        private int defence;
+        private int range;
+        private int damage;
+        private int speed;
+
+        public int Hp { get => hp; set => hp = value; }
+        public int Defence { get => defence; set => defence = value; }
+        public int Range { get => range; set => range = value; }
+        public int Damage { get => damage; set => damage = value; }
+        public int Speed { get => speed; set => speed = value; }
+
+        public Unit(int hp, int defence, int range, int damage, int speed)
+        {
+            this.hp = hp;
+            this.defence = defence;
+            this.range = range;
+            this.damage = damage;
+            this.speed = speed;
+        }
     }
 }
