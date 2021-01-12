213750b0357456001e85ae32a1e6fa2d413043c7
Domik0 <dafedot1@gmail.com>
2021-01-12 16:46:58 +0300

    lesson end 12.01.2021

---
+++ 321labs/Fedotov/Game/Unit/WeaponsStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Fedotov.Game
+{
+    class WeaponsStorage : IStorage
+    {
+        public int durability { get; set; }
+        public int destroyed { get; set; }
+        public int capacity { get; set; }
+        public string typeWeapons;
+    }
+}
