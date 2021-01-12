fe8d53546cfc50c5a2f94dc73cea13f9fe282ee1
Булат Камалов <kamalov_bulat2002@mail.ru>
2021-01-12 16:46:08 +0300

    lesson end 12.01.21

--- 321labs/Kamalov/Game/Hero.cs
+++ 321labs/Kamalov/Game/Hero.cs
@@ -7,5 +7,16 @@ namespace _321labs.Kamalov.Game
     class Hero
     {
         int Health { get; set; }
+        int Armor { get; set; }
+        int Speed { get; set; }
+        string Name { get; set; }
+
+        public Hero(int health, int armor, int speed, string name)
+        {
+            Health = health;
+            Armor = armor;
+            Speed = speed;
+            Name = name;
+        }
     }
 }
