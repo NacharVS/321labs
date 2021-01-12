f37a36e100daa22ec166ea768ed802889833716f
Diistaf <kikopoint@mail.ru>
2021-01-12 16:09:03 +0300

    Create heroes

---
+++ 321labs/Game/Archer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Game
+{
+    class Archer : Unit
+    {
+        public Archer(int hp, int defence, int range, int damage, int speed) : base(hp, defence, range, damage, speed)
+        {
+
+        }
+    }
+}
---
+++ 321labs/Game/Tank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Game
+{
+    class Tank : Unit
+    {
+        public Tank(int hp, int defence, int range, int damage, int speed) : base(hp, defence, range, damage, speed)
+        {
+
+        }
+    }
+}
