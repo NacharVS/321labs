5d822daf0fa927c8163eeb9600bfbc14fefec47e
pear <dashssm0@gmail.com>
2021-01-12 16:26:55 +0300

    lab1 start lesson

---
+++ 321labs/Game/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Game
+{
+    class Unit
+    {
+       private int healthPoints;
+        private int power;
+        private int speed;
+
+    }
+}
