abf8aad2326db1d69588ca1df41afe18e11accf2
nectarine <name.ramil.aq@mail.ru>
2021-01-12 16:47:23 +0300

    OnLession 12.01.2021

--- 321labs/Game/Hero.cs
+++ 321labs/Game/Hero.cs
@@ -6,6 +6,20 @@ namespace _321labs.Game
 {
     class Hero
     {
-        public int damage;
+        public int Damage { get; set; }
+        public int Health { get; set; }
+        public int Speed { get; set; }
+        public int Range { get; set; }
+        public int Visibility { get; set; }
+
+        public Hero(int damage, int health, int speed, int range, int visibility)
+        {
+            Damage = damage;
+            Health = health;
+            Speed = speed;
+            Range = range;
+            Visibility = visibility;
+        }
+
     }
 }
