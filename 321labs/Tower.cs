e38b1ea8fead7935fc4d5c1aaed59bc9fadd5663
Ranil777 <x-ranil2002@mail.ru>
2021-01-12 16:44:04 +0300

    end lesson 12.01.21

---
+++ 321labs/Tower.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs
+{
+    class Tower: Unit
+    {
+        int armor = 10;
+        int damage = 0;
+        int range = 6;
+        int sight = 10;
+        int speed = 0;
+    }
+}
