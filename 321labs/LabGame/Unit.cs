888fd42dd3000634f38ee058310dacae8d92ada9
Kamikotoro <nanano102030@mail.ru>
2021-01-12 16:35:14 +0300

    OnLesson lab1 Game Upd1

--- 321labs/LabGame/Unit.cs
+++ 321labs/LabGame/Unit.cs
@@ -7,7 +7,12 @@ namespace _321labs.LabGame
     abstract class Unit
     {
         public abstract position UnitPosition { get; set; }
+        public abstract float HealthPoint { get; set; }
+        public abstract float Defense { get; set; }
+        public abstract float Attack { get; set; }
 
-        
+        public abstract float Range { get; set; }
+
+        public abstract bool InRange(position position);
     }
 }
