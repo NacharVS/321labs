77f424b33052dbdb6f14e9a07bc32cee9abed730
orange <plazma.sphere@yandex.ru>
2021-01-12 16:37:28 +0300

    IUnit has been created

---
+++ 321labs/IUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs
+{
+    interface IUnit
+    {
+        string Name { get; set; }
+        int Hp { get; set; }
+        int Armor { get; set; }
+    }
+}
