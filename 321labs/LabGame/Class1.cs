93cc3db90dea02e6b67e77ddeebae11cea752df4
Kamikotoro <nanano102030@mail.ru>
2021-01-12 16:22:39 +0300

    lesson lab1 game start

---
+++ 321labs/LabGame/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.LabGame
+{
+    public struct position
+    {
+        public float x;
+        public float y;
+    }
+}
---
+++ 321labs/LabGame/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.LabGame
+{
+    abstract class Unit
+    {
+        public abstract position UnitPosition { get; set; }
+
+        
+    }
+}
