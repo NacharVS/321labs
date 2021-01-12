cc774bef67fb4c1950557e992e29635cbfe92cd6
r3dinc <r3dinc@yandex.ru>
2021-01-12 14:35:17 +0300

    lesson end 12.01.2021

--- 321labs/Nacharov/threadingLab.cs
+++ 321labs/Nacharov/threadingLab.cs
@@ -1,6 +1,12 @@
-﻿namespace _321labs.Nacharov
+﻿using System.Threading;
+
+namespace _321labs.Nacharov
 {
     class threadingLab
     {
+        static void Task1()
+        {
+            Thread thread;
+        }
     }
 }
