29986e92c17bc24bed607588885f356a7556867f
r3dinc <r3dinc@yandex.ru>
2021-01-12 14:07:34 +0300

    created

---
+++ 321labs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _321labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leave hope behind all who enters here...");
+        }
+    }
+}
