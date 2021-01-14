0e577f916d27a34b83d1f188c0db5f8bb4fd8082
SirHurtAlot1 <Sudakov.valera@list.ru>
2021-01-14 13:52:36 +0300

    14.01 lesson end

---
+++ 321labs/Threads/Threads.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _321labs.Treads
+{
+    class Threads
+    {
+        int[] arr;
+        Thread summThread;
+        Thread maxElemThread;
+        Thread sortingThread;
+        public int[] Arr { get => arr; set => arr = value; }
+
+        public Threads(int[] array)
+        {
+            this.arr = array;
+        }
+
+        public void StartThreads()
+        {
+            
+        }
+
+        void ElementSum()
+        {
+
+        }
+    }
+}
