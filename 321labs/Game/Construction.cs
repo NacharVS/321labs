6be1e3a858d11e58a358990620f64ddb72208d12
Diistaf <kikopoint@mail.ru>
2021-01-14 09:48:55 +0300

    14.01.2021

---
+++ 321labs/Game/Construction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Game
+{
+    class Construction
+    {
+        public int Health { get; set; }
+        public int Defence { get; set; }
+        public int Position { get; set; }
+
+
+    }
+}
---
+++ 321labs/Game/Tower.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Game
+{
+    class Tower : Construction
+    {
+
+    }
+}
