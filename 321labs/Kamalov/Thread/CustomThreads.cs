4725ca8780783f2dda3c5596b3c8c79df81f83e3
CIDEY <kamalov_bulat2002@mail.ru>
2021-01-14 21:33:49 +0300

    Thread Final Fix

--- 321labs/Kamalov/Thread/CustomThreads.cs
+++ 321labs/Kamalov/Thread/CustomThreads.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 
 namespace _321labs.Kamalov.Threads
@@ -15,7 +13,7 @@ namespace _321labs.Kamalov.Threads
         {
 
         }
-
+        
         public void Total(object obj)
         {
             int[] vs = (int[])obj;
@@ -65,6 +63,22 @@ namespace _321labs.Kamalov.Threads
                 Console.WriteLine($"Total sort - {mass[i]}");
                 Thread.Sleep(55);
             }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 3123, 165, 2123, 464, 7, -22, 512 };
+
+            CustomThreads arr = new CustomThreads();
+            Thread test1 = new Thread(new ParameterizedThreadStart(arr.Total));
+            Thread test2 = new Thread(new ParameterizedThreadStart(arr.Maximum));
+            Thread test3 = new Thread(new ParameterizedThreadStart(arr.Sort));
+
+            test1.Start(test);
+            test2.Start(test);
+            test3.Start(test);
         }
     }
 }
--- 321labs/Kamalov/Thread/Program.cs
+++ 321labs/Kamalov/Thread/Program.cs
@@ -1,25 +1,22 @@
-﻿using _321labs.Kamalov.Threads;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+﻿//using _321labs.Kamalov.Threads;
+//using System.Threading;
 
-namespace _321labs.Kamalov.Thread
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] test = { 3123, 165, 2123, 464, 7, -22, 512 };
+//namespace _321labs.Kamalov.Thread
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] test = { 3123, 165, 2123, 464, 7, -22, 512 };
 
-            CustomThread arr = new CustomThread();
-            Thread test1 = new Thread(new ParameterizedThreadStart(arr.Total));
-            Thread test2 = new Thread(new ParameterizedThreadStart(arr.Maximum));
-            Thread test3 = new Thread(new ParameterizedThreadStart(arr.Sort));
+//            CustomThreads arr = new CustomThreads();
+//            Threads test1 = new Threads(new ParameterizedThreadStart(arr.Total));
+//            Threads test2 = new Threads(new ParameterizedThreadStart(arr.Maximum));
+//            Threads test3 = new Threads(new ParameterizedThreadStart(arr.Sort));
 
-            test1.Start(test);
-            test2.Start(test);
-            test3.Start(test);
-        }
-    }
-}
+//            test1.Start(test);
+//            test2.Start(test);
+//            test3.Start(test);
+//        }
+//    }
+//}
