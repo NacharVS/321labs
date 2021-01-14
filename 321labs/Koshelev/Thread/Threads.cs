715397064cf2966dd039c8ca840220a009986c08
Rtakale <dialogbgr@mail.ru>
2021-01-14 11:10:22 +0300

    Thread Start

---
+++ 321labs/Koshelev/Thread/Threads.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Koshelev.Thread
+{
+    class Threads
+    {
+        int[] intNums;
+
+        public Threads(int[] intNums)
+        {
+            this.intNums = intNums;
+        }
+
+        public void SortMas()
+        {
+            Array.Sort(intNums);
+            foreach(int i in intNums)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Max()
+        {
+            int max = intNums[0];
+            foreach (int i in intNums)
+            {
+                if (max < i)
+                    max = i;
+            }
+            Console.WriteLine("Max = " + max);
+        }
+
+        public void SumMas()
+        {
+            int sum = 0;
+            foreach(int i in intNums)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Summa mas = " + sum);
+        }
+    }
+}
--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Threading;
+using _321labs.Game;
+using _321labs.Koshelev.Thread;
 
-namespace _321labs.Game
+namespace _321labs
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Leave hope behind all who enters here....");
+            int[] mas = { 1, 2, 4, 63, 4, 85, 3, 6, 9, 6, 3, 5, 9, 35, 6 };
+            Threads threads = new Threads(mas);
+            Thread thread = new Thread(new ThreadStart(threads.Max));
+            thread.Start();
+            thread = new Thread(new ThreadStart(threads.SortMas));
+            thread.Start();
+            thread = new Thread(new ThreadStart(threads.SumMas));
+            thread.Start();
+
+
 
         }
     }
