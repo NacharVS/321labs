27076b1249a8ca0803c71464de67a8bb01cd308a
Kamikotoro <nanano102030@mail.ru>
2021-01-14 11:11:11 +0300

    LessonEnd lab2 Threads

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -1,39 +1,12 @@
 ﻿using System;
-using _321labs.LabGame.Base;
-using _321labs.LabGame.Heroes;
-using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace _321labs
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Leave hope behind all who enters here....");
-            Warrior warrior1 = new Warrior(new Vector2(8, 8), 1, "Элезар", 100,20, 1, 2, 20, 10, 24);
-            Warrior warrior2 = new Warrior(new Vector2(-8, -8), 1, "Артур", 100, 20, 1, 2, 20, 4, 4);
-
-            Console.WriteLine(Math.Sqrt(Math.Pow((warrior1.UnitPosition.X - warrior2.UnitPosition.X), 2) + Math.Pow((warrior1.UnitPosition.Y - warrior2.UnitPosition.Y), 2)));
-
-            warrior1.ScanAround();
-
-            warrior2.ScanAround();
-
-
-            Console.WriteLine(warrior1.UnitPosition);
-            Vector2 position = new Vector2(-9, -9);
-
-            while (warrior1.UnitPosition != position)
-            {
-                warrior1.MoveToPoint(position);
-                Console.WriteLine(warrior1.UnitPosition);
-            }
-
-            Console.WriteLine(warrior2.HealthPoints);
-
-            warrior1.AttackToPoint(warrior2.UnitPosition);
-            
-            Console.WriteLine(warrior2.HealthPoints);
-        }
+        
     }
 }
---
+++ 321labs/Threads/ThreadsProgram.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace _321labs.Threads
+{
+    class ThreadsProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 1, 2, 33, 44, 5, 3, 2, 4, 2, 4 };
+            ArrayThreadData arrData = new ArrayThreadData(arr);
+            Thread SumThread = new Thread(SumIntArray);
+            Thread MaxThread = new Thread(MaxIntArray);
+            Thread SortThread = new Thread(SortIntArray);
+            SumThread.Start(arrData);
+            MaxThread.Start(arrData);
+            SortThread.Start(arrData);
+        }
+
+        static void SumIntArray(object obj)
+        {
+            if (obj is ArrayThreadData arrData)
+            {
+                int sum = 0;
+                foreach (int i in arrData.arr)
+                {
+                    sum += i;
+                }
+                Console.WriteLine($"Sum Thread Work Result = {sum}");
+            }
+        }
+
+        static void MaxIntArray(object obj)
+        {
+            if (obj is ArrayThreadData arrData)
+            {
+                int max = arrData.arr[0];
+                foreach (int i in arrData.arr)
+                {
+                    if (max < i)
+                    {
+                        max = i;
+                    }
+                }
+                Console.WriteLine($"Max Thread Work Result = {max}");
+            }
+        }
+
+        static void SortIntArray(object obj)
+        {
+            if (obj is ArrayThreadData arrData)
+            {
+                Array.Sort(arrData.arr);
+                foreach (int i in arrData.arr)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+
+        private class ArrayThreadData
+        {
+            public int[] arr;
+
+            public ArrayThreadData(int[] arr)
+            {
+                this.arr = arr;
+            }
+        }
+    }
+}
