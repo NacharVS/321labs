591b2e5f3349120d5c76f3ea77f873433922e993
lemon <daniil2015473@gmail.com>
2021-01-14 11:10:30 +0300

    Lesson end

--- 321labs/Interface/ICommonUnit.cs
+++ 321labs/Interface/ICommonUnit.cs
@@ -6,12 +6,12 @@ namespace _321labs
 {
     interface ICommonUnit
     {
+        double Cost { get; set; }
         string Name { get; set; }
         int Hp { get; set; }
         int Strenght { get; set; }
         bool IsMove { get; set; }
         int Armor { get; set; }
         double Evasion { get; set; }
-
     }
 }
