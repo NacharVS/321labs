1486e6d2bf105c1ee6981654782ff6b06ed836ce
Domik0 <dafedot1@gmail.com>
2021-01-14 11:12:54 +0300

    Lab 14.01.2021 Finish

---
+++ 321labs/Fedotov/Threads.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Fedotov
+{
+    class Threads
+    {
+        int[] array;
+
+        public Threads(int[] mass)
+        {
+            array = mass;
+        }
+
+        public void Start()
+        {
+            Thread threadMax = new Thread(new ThreadStart(Max));
+            Thread threadSum = new Thread(new ThreadStart(Sum));
+            Thread threadSort = new Thread(new ThreadStart(Sort));
+            threadMax.Start();
+            threadSum.Start();
+            threadSort.Start();
+        }
+
+        void Max()
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                    max = array[i];
+            }
+
+            Console.WriteLine("Max="+max);
+        }
+
+        void Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+                sum += array[i];
+            Console.WriteLine("Sum=" + sum);
+        }
+
+        void Sort()
+        {
+            Array.Sort(array);
+            foreach (int item in array)
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using _321labs.Fedotov.Game;
+using _321labs.Fedotov;
 
 namespace _321labs
 {
@@ -6,7 +8,9 @@ namespace _321labs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Я без понятия правильно это или нет, я не мб это все вообще не правильно");
+            //Console.WriteLine("Я без понятия правильно это или нет, я не мб это все вообще не правильно");
+            Threads threads = new Threads(new []{5,5,6,8,9,8,9,5,9,84,4,1,9,4,6,9});
+            threads.Start();
         }
     }
 }
