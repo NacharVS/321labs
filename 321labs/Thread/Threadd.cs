bf2a627e7a2c35df25c6fae7b3ffb9256394e02a
nectarine <name.ramil.aq@mail.ru>
2021-01-14 10:54:27 +0300

    Lab2 start

---
+++ 321labs/Thread/Threadd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Thread
+{
+    class Threadd
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
