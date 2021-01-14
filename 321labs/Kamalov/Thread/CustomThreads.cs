dcd823f70cc27b1dbea916abef5d1369d3c7e545
CIDEY <kamalov_bulat2002@mail.ru>
2021-01-14 19:55:15 +0300

    CustomThread part1

---
+++ 321labs/Kamalov/Thread/CustomThreads.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Kamalov.Threads
+{
+    class CustomThreads
+    {
+        int amount;
+
+        public CustomThreads()
+        {
+
+        }
+
+        public void Total(object obj)
+        {
+            int[] vs = (int[])obj;
+
+            for (int i = 0; i < vs.Length; i++)
+            {
+                amount += vs[i];
+                Console.WriteLine($"Total: {amount}");
+            }
+            Console.WriteLine($"All Total {amount}");
+            Thread.Sleep(50);
+        }
+    }
+}
