53f051a468bda99190b51185ea0a115c82b4a568
Diistaf <kikopoint@mail.ru>
2021-01-14 11:04:53 +0300

    Thread start

---
+++ 321labs/Thread/Threads.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Thread
+{
+    class Threads
+    {
+        int[] numbers = { 2, 5, 4, 1, 10 };
+
+        public int Sum()
+        {
+            int sum = 0;
+            foreach (int item in numbers)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        public int Max()
+        {
+            int max = 0;
+            foreach (int item in numbers)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+        public void SortArray()
+        {
+            Array.Sort(numbers);
+        }
+
+        public static void Start()
+        {
+            Thread potok1 = new Thread(Sum);
+        }
+    }
+}
