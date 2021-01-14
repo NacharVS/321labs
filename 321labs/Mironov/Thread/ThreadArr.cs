53bb3e14d749e58dcab7a76bb74ea79a031400b3
DefoltFES <mironoff.emil@yandex.ru>
2021-01-14 10:51:44 +0300

    Begin Lesson

---
+++ 321labs/Mironov/Thread/ThreadArr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Mironov.Thread
+{
+    class ThreadArr
+    {
+        private int[] arr;
+
+        public ThreadArr(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+    }
+}
