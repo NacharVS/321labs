106a389cdab6db93e74bfb7b9eee4d876d773b95
Rileaze <faizraxmanovelik@gmail.com>
2021-01-14 11:10:51 +0300

    end lesson

---
+++ 321labs/Game/threads/threads(massiov).cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Game.threads
+{
+
+    public class threadsmassivSUm
+    {
+        int[] numbers = new int[]
+        {
+            0,1,2,3,4,5,6,7,8,9,10
+        };
+        int sum;
+       
+        foreach (int value in numbers)
+            {
+            
+            }
+
+    }
+    
+
+}
