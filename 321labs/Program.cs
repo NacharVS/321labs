8ec8c5ed5fcb011d094e2e6c065d6e57cd9c19ab
Ильнар Файзрахманов <faizraxmanovelik@gmail.com>
2021-01-14 14:13:04 +0300

    Home work no finish  fix later 14.01.21

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using _321labs.Treads;
 
 namespace _321labs
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Leave hope behind all who enters here....");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			
+			
+		}
+	}
 }
---
+++ 321labs/threads/threads(massiov).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace _321labs.Treads
+{
+    class Threads
+    {
+        int[] array;
+
+        public Threads(int[] mass)
+        {
+            array = mass;
+        }
+        void Max()
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                    max = array[i];
+            }
+
+            Console.WriteLine("Max=" + max);
+        }
+
+        void Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+                sum += array[i];
+            Console.WriteLine("Sum=" + sum);
+        }
+
+        void Sort()
+        {
+            Array.Sort(array);
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+     
+    }
+}
