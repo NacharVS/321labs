afd80d2cd5ced52f8f8ef623c17c3ef659533c7c
avocado <arhipovaanastasia16@gmail.com>
2021-01-14 11:27:47 +0300

    lab2 LessonEnd Threading

---
+++ 321labs/Threads/ThreadsProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Threads
+{
+    class ThreadsProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leave hope behind all who enters here....");
+        }
+    }
+}
