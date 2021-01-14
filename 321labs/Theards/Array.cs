dc91fd93015c3fda0667fd3f032752f421dd186f
banana <dany_021270@mail.ru>
2021-01-14 11:10:44 +0300

    14.01.2021 11:10 neponyal che delat

---
+++ 321labs/Theards/Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Theards
+{
+    class Array
+    {
+        private int[] arr;
+
+        public int[] Arr
+        {
+            get;
+            set;
+        }
+        public void Threads()
+        {
+            Thread thread = new Thread();
+        }
+        static void Max()
+        {
+           
+        }
+
+
+    }
+}
