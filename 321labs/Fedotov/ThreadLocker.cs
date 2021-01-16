25ff6adcf59b0451113516ef0d44b8f80af25ed6
Domik0 <dafedot1@gmail.com>
2021-01-16 10:40:48 +0300

    Start ThreadLocker

---
+++ 321labs/Fedotov/ThreadLocker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs.Fedotov
+{
+    class ThreadLocker
+    {
+        static int[] array1;
+        static int[] array2;
+        static int[] array3;
+
+        static public void Start()
+        {
+            Thread thread1 = new Thread(() => GenericArray(array1)); 
+            Thread thread2 = new Thread(() => GenericArray(array2));
+            Thread thread3 = new Thread(() => Summ(array1, array2));
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        void GenericArray(int[] mass)
+        {
+            lock (new object())
+            {
+                Random rnd = new Random();
+                mass = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    mass[i] = rnd.Next(0, 100);
+                }
+            }
+        }
+
+        
+    }
+}
--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -9,8 +9,8 @@ namespace _321labs
         static void Main(string[] args)
         {
             //Console.WriteLine("Я без понятия правильно это или нет, я не мб это все вообще не правильно");
-            Threads threads = new Threads(new []{5,5,6,8,9,8,9,5,9,84,4,1,9,4,6,9});
-            threads.Start();
+            ThreadLocker threads = new ThreadLocker();
+            ThreadLocker.Start();
         }
     }
 }
