d560fb9b17cb65007694eaffabfde9fb9383919d
Ydin321 <andrey302230221@gmail.com>
2021-01-17 20:43:36 +0300

    3labka 2 commit

--- 321labs/Ydin321/3Labka/Locker.cs
+++ 321labs/Ydin321/3Labka/Locker.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace _321labs.Ydin321._3Labka
 {
     class Locker
     {
+        private static object locker = new object();
+
+        static int[] ArrNumbOne = new int[10];
+        static int[] ArrNumbTwo = new int[10];
+        static int[] sum = new int[10];
+
+        public void First()
+        {
+            lock (locker)
+            {
+                Random rand = new Random();
+
+                for (int i = 0; i < ArrNumbOne.Length; i++)
+                {
+                    ArrNumbOne[i] = rand.Next(0, 20);
+                    Console.WriteLine($"{i + 1} элемент первого массива {ArrNumbOne[i]}");
+                    Thread.Sleep(10);
+                }
+                Console.WriteLine();
+            }
+        }
+      
     }
 }
+
