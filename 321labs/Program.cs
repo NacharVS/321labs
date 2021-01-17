96f7a3570be75cd9463ac9ee7bf2297fc6dbc2e4
Ильнар Файзрахманов <faizraxmanovelik@gmail.com>
2021-01-17 11:33:33 +0300

    final lab3 synh thread 17.01.21 11;33

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -1,14 +1,13 @@
 ﻿using _321labs.Treads;
-
+using _321labs.Synh;
 namespace _321labs
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-
-			Threads threads = new Threads(new[] { 15, 35, 6, 28, 49, 58, 19, 35, 9, 84, 4, 1, 9, 4, 16, 5 });
-			threads.Start();
+			
+			
 
 		}
 	}
--- 321labs/threadsSynh/massiv.cs
+++ 321labs/threadsSynh/massiv.cs
@@ -7,14 +7,77 @@ namespace _321labs.Synh
     {
         static int[] array = new int[10];
         static int[] array2 = new int[10];
-        static int[] resultArray = new int[10];
+        static int[] sumArray = new int[10];
         object x = new object();
+        object f = new object();
+        object b = new object();
 
-      
+        public void GenerateArray1()
+        {
+            lock (x)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rnd.Next(1, 20);
+                }
+                Console.WriteLine("Первый массив");
+                foreach (var item in array)
+                {
+                    Console.WriteLine(item + " ");
+
+                }
+
+            }
         }
+        public void GenerateArray2()
+        {
+            lock (b)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rnd.Next(1, 20);
+                }
+                Console.WriteLine("Второй массив");
+                foreach (var item in array2)
+                {
+                    Console.WriteLine(item + " ");
+                }
 
+
+            }
+        }
+       public void Sum()
+        {
+            lock (f)
+            {
+                for (int i = 0; i < sumArray.Length; i++)
+                {
+                    sumArray[i] = array[i] + array2[i];
+
+                }
+                Console.WriteLine("Сумма");
+            }
+            foreach (var item in sumArray)
+            {
+                Console.WriteLine(item+" ");
+            }
+
+        }
+        public void start()
+        {
+            Thread thread1 = new Thread(GenerateArray1);
+            Thread thread2 = new Thread(GenerateArray2);
+            Thread thread3 = new Thread(Sum);
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
     }
+
 }
+
 
 
 
