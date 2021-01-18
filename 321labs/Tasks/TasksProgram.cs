63402412febb163fb133851b1c1cb5f5c3c8319a
avocado <arhipovaanastasia16@gmail.com>
2021-01-18 10:57:55 +0300

    lab 3 18.01.2021

--- 321labs/Tasks/TasksProgram.cs
+++ 321labs/Tasks/TasksProgram.cs
@@ -2,35 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace _321labs.Threads
+namespace _321labs.Tasks
 {
-    class ThreadsProgram
+    class TasksProgram
     {
         static void Main(string[] args)
         {
-            int[] a =  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        }
-        class Class2
-        {
-            public int[] a;
-            public Class2(int [] a)
-            {
-                this.a = a;
-            }
-        }
-        static void Tr()
-        {
-            Thread Thread1 = new Thread(new ParameterizedThreadStart(Summ));
-            Thread1.Start();
-            Thread Thread2 = new Thread(new ParameterizedThreadStart(Max));
-            Thread2.Start();
-            Thread Thread3 = new Thread(new ParameterizedThreadStart(Sort));
-            Thread3.Start();
+            int[] s = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Task task1 = new Task(() => { });           
         }
         static void Summ(object obj)
         {
-            if (obj is  Class2 class2 )
+            if (obj is Class2 class2)
             {
                 int summ = class2.a[0];
                 foreach (var item in class2.a)
@@ -45,7 +30,7 @@ namespace _321labs.Threads
             if (obj is Class2 class2)
             {
                 int max = class2.a[0];
-                foreach (var item in class2.a )
+                foreach (var item in class2.a)
                 {
                     max = item;
                 }
@@ -62,9 +47,7 @@ namespace _321labs.Threads
                 {
                     Console.WriteLine($"Сортировка: {item + " "}");
                 }
-               
+
             }
         }
-
     }
-}
