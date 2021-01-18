a5c151bbe861fce730aaa83e7b7c9fb5762ea3a6
Rtakale <dialogbgr@mail.ru>
2021-01-18 10:55:11 +0300

    Start lesson Tasks

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using _321labs.Koshelev.Game;
 using _321labs.Koshelev.Thread;
 using _321labs.Game;
@@ -8,52 +9,20 @@ namespace _321labs
 {
     class Program
     {
-        static int[] mas1 = new int[10];
-        static int[] mas2 = new int[10];
-        static int[] mas3 = new int[10];
-        static object locker = new object();
+        static int[] mas1 = { 1, 5, 89, 6, 11, 4, 7, 9, 7, 12, 3, 6, 8, 44 };
 
         static void Main(string[] args)
         {
-            
-            Warrior warrior = new Warrior();
-            Tower tower = new Tower();
-            warrior.Attack(tower);
-            tower.Blocking();
-            Barracks barracks = new Barracks();
-            barracks.Notification();
-            
-        }
-        static void RandomIntNumForMas1(int[] mas)
-        {
-            Random random = new Random();
-            lock (locker)
-            {
-                for (int i = 0; i < mas.Length; i++)
-                {
-                    mas[i] = random.Next(0, 50);
-                }
-            }
-        }
-        static void RandomIntNumForMas2(int[] mas)
-        {
-            Random random = new Random();
-            lock (locker)
-            {
-                for (int i = 0; i < mas.Length; i++)
-                {
-                    mas[i] = random.Next(0, 50);
-                }
-            }
-        }
-        static void SumMas()
-        {
-            Console.Write("MasResult: ");
-            for (int i = 0; i < mas3.Length; i++)
-            {
-                mas3[i] = mas1[i] + mas2[i];
-                Console.Write(mas3[i] + ", ");
-            }
+            Threads threads = new Threads(mas1);
+            Task task = new Task(threads.Max);
+            Task task1 = new Task(threads.SortMas);    
+            Task task2 = new Task(threads.SumMas);
+            task.Start();
+            task1.Start();
+            task2.Start();
+            task.Wait();
+            task1.Wait();
+            task2.Wait();
         }
     }
 }
