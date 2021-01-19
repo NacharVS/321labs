387f9016afc9ca8826a1755c6ef6b1d32d3c2da4
nectarine <name.ramil.aq@mail.ru>
2021-01-19 11:32:56 +0300

    Tasks finish

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using _321labs;
 
 namespace _321labs
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+        }
     }
 }
