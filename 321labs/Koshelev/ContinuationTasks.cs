1a640dc61fabf563aa23a87c881ae2d1be28e3c4
Rtakale <dialogbgr@mail.ru>
2021-01-19 12:54:26 +0300

    Start lesson ContinuationTasks

---
+++ 321labs/Koshelev/ContinuationTasks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs.Koshelev
+{
+    class ContinuationTasks
+    {
+        int[] mas;
+
+        public int[] Mas { get => mas; set => mas = value; }
+
+        public ContinuationTasks(int[] mas)
+        {
+            this.Mas = mas;
+        }
+
+        public void RandomElevent()
+        {
+            Random random = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = random.Next(0, 50);
+            }
+        }
+
+        public void 
+    }
+}
--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -9,22 +9,13 @@ namespace _321labs
 {
     class Program
     {
-        static int[] mas1 = { 1, 5, 89, 6, 11, 4, 7, 9, 7, 12, 3, 6, 8, 44 };
+        static int[] mas2 = new int[];
 
         static void Main(string[] args)
         {
-            //
-            Threads threads = new Threads(mas1);
-            Task task = new Task(threads.Max);
-            Task task1 = new Task(threads.SortMas);    
-            Task task2 = new Task(threads.SumMas);
-            task.Start();
-            task1.Start();
-            task2.Start();
-            task.Wait();
-            task1.Wait();
-            task2.Wait();
-            //
+            Task task1 = new Task()
         }
+
+       
     }
 }
