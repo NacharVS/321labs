6cc635b53ff934ff8b4a19b032ed15fc4cc65333
nectarine <name.ramil.aq@mail.ru>
2021-01-19 13:20:49 +0300

    ContinuationTasks end

--- 321labs/ContinuationTasks/ContinuationTasks.cs
+++ 321labs/ContinuationTasks/ContinuationTasks.cs
@@ -16,34 +16,41 @@ namespace _321labs.ContinuationTasks
             for (int i = 0; i < mas.Length; i++)
             {
                 mas[i] = r.Next(1, 5);
-                Thread.Sleep(500);
+                // Thread.Sleep(500);
             }
-            Task TaskProduct = new Task(() =>
-            {
-                int a = 0;
-                for (int i = 0; i < mas.Length; i++)
-                {
-                    Console.WriteLine("Сумма элементов:");
-                    a *= mas[i];
-                    Console.WriteLine(a);
-                    Thread.Sleep(1000);
-                }
-                Console.WriteLine();
-            });
-            Task TaskEven = new Task(() =>
-            {
-                int a = 0;
-                for (int i = 0; i < mas.Length; i++)
-                {
-                    Console.WriteLine("Сумма элементов:");
-                    if ((mas[i]%2)==0)
-                    {
-                        Console.WriteLine(a);
-                        Thread.Sleep(1000);
-                    }
-                }
-                Console.WriteLine();
-            });
+            //    Task TaskProduct = new Task(() =>
+            //    {
+            //        int a = 0;
+            //        for (int i = 0; i < mas.Length; i++)
+            //        {
+            //            Console.WriteLine("Сумма элементов:");
+            //            a *= mas[i];
+            //            Console.WriteLine(a);
+            //           // Thread.Sleep(1000);
+            //        }
+            //        Console.WriteLine();
+            //    });
+            //    //Task TaskEven = new Task(() =>
+            //    //{
+            //    //    int a = 0;
+            //    //    for (int i = 0; i < mas.Length; i++)
+            //    //    {
+            //    //        Console.WriteLine("Сумма элементов:");
+            //    //        if ((mas[i]%2)==0)
+            //    //        {
+            //    //            Console.WriteLine(a);
+            //    //            Thread.Sleep(1000);
+            //    //        }
+            //    //    }
+            //    //    Console.WriteLine();
+            //    //});
+            //});
         });
+        Task task2 = .ContinueWith(Display);
+
+        TaskGenerete.Start();
+             
+
+        TaskEven.Wait();
     }
 }
