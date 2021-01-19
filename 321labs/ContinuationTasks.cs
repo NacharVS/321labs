d170bf086f8acda34773fbba881b3264c4f5f8a7
SirHurtAlot1 <Sudakov.valera@list.ru>
2021-01-19 13:19:40 +0300

    13:19 end  of lesson(not finished)

---
+++ 321labs/ContinuationTasks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs
+{
+    class ContinuationTasks
+    {
+        int[] array = new int[20];
+        int product = 1;
+        
+        public void GenerateNumbers()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 30);
+            }
+            foreach (var item in array)
+            {
+                Console.Write(item+" ");
+            }
+        }
+        public void ElementProduct()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                product *= array[i];
+            }
+            Console.WriteLine($"Произведение: {product}");
+        }
+
+        public void EvenFromProduct()
+        {
+            string number = Convert.ToString(product);
+
+            //double buffer;
+            //List<double> evenNumb = new List<double>();
+            //while (number!=0)
+            //{
+            //    buffer = number % 10;
+            //    if (buffer%2 == 0)
+            //    {
+            //        evenNumb.Add(buffer);
+            //        number -= buffer;
+            //    }
+            //    else
+            //    {
+            //        number -= buffer;
+            //    }
+            //}
+            //evenNumb.Reverse();
+            //foreach (var item in evenNumb)
+            //{
+            //    Console.WriteLine(item+" ");
+            //}
+        }
+    }
+}
