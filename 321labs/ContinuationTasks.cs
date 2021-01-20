985983b56568c3da66414d676c6e7c6076467aec
pomegranate <ilya_shmelev2002@mail.ru>
2021-01-20 18:41:50 +0300

    start Continuation asks

---
+++ 321labs/ContinuationTasks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs
+{
+    class ContinuationTasks
+    {
+
+        static int[] numbers = new int[20];
+        public static void CreateArray()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(1,20);
+            }
+            Console.WriteLine("Array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+
+        public static void GetEvenNumvers()
+        {
+            Console.WriteLine("Even numbers:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if((numbers[i] % 2) == 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -8,16 +8,7 @@ namespace _321labs
     {
         static void Main(string[] args)
         {
-            Task t1 = new Task(Tasks.Max);
-            Task t2 = new Task(Tasks.Sum);
-            Task t3 = new Task(Tasks.SortArray);
-            t1.Start();
-            t2.Start();
-            t3.Start();
-
-            t1.Wait();
-            t2.Wait();
-            t3.Wait();
+            
         }
     }
 }
