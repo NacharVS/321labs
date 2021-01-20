37bcf679cbba748151f5bdae3a2d90a74d99f22e
Ydin321 <andrey302230221@gmail.com>
2021-01-20 20:11:14 +0300

    4 labka 2 commit

--- 321labs/Ydin321/4labka/ContinTasks.cs
+++ 321labs/Ydin321/4labka/ContinTasks.cs
@@ -7,6 +7,39 @@ namespace _321labs.Ydin321._4labka
 {
     class ContinTasks
     {
+        private static long[] arr = new long[20];
+        static Random rnd = new Random();
+        public static void Start()
+        {
+            Task task1 = new Task(GenerateValue);
+            Task<long> task2 = task1.ContinueWith(m => MultiplyElem());
+            Task task3 = task2.ContinueWith(m => ShowEven(task2.Result));
+            task1.Start();
+            task3.Wait();
 
+        }
+        private static void GenerateValue()
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 10);
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static long MultiplyElem()
+        {
+            long product = 1;
+            foreach (var item in arr)
+            {
+                product *= item;
+            }
+            return product;
+        }
     }
 }
