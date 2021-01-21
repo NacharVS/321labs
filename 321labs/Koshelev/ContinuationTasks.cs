ee2e6d0373e8f2f6a85ce1426774fc362fc5622e
Rtakale <dialogbgr@mail.ru>
2021-01-21 09:50:09 +0300

    Finish ContinuationTasks

--- 321labs/Koshelev/ContinuationTasks.cs
+++ 321labs/Koshelev/ContinuationTasks.cs
@@ -6,24 +6,44 @@ namespace _321labs.Koshelev
 {
     class ContinuationTasks
     {
-        int[] mas;
-
+        int[] mas = new int[20];
+        long result;
         public int[] Mas { get => mas; set => mas = value; }
-
-        public ContinuationTasks(int[] mas)
-        {
-            this.Mas = mas;
-        }
 
         public void RandomElevent()
         {
+            Console.Write("Массив");
             Random random = new Random();
             for (int i = 0; i < mas.Length; i++)
             {
-                mas[i] = random.Next(0, 50);
+                mas[i] = random.Next(1, 10);
+                Console.Write(mas[i] + ", ");
             }
         }
 
-        public void 
+        public void ProductMas()
+        {
+            result = mas[0];
+            for (int i = 1; i < mas.Length; i++)
+            {
+                result *= mas[i];
+            }
+
+        }
+
+        public void EvenNumbers()
+        {
+            string resultS = result.ToString();
+            Console.WriteLine("Результат = " + result);
+            for (int i = 0; i < resultS.Length; i++)
+            {
+                if (Convert.ToInt32(resultS[i]) % 2 == 0)
+                {
+                    Console.WriteLine(resultS[i]);
+                }
+           
+            }
+        }
+
     }
 }
--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -3,17 +3,23 @@ using System.Threading;
 using System.Threading.Tasks;
 using _321labs.Koshelev.Game;
 using _321labs.Koshelev.Thread;
+using _321labs.Koshelev;
 using _321labs.Game;
 
 namespace _321labs
 {
     class Program
     {
-        static int[] mas2 = new int[];
 
         static void Main(string[] args)
         {
-            Task task1 = new Task()
+            ContinuationTasks continuationTasks = new ContinuationTasks();
+            Task task1 = new Task(continuationTasks.RandomElevent);
+            Task task2 = task1.ContinueWith(t => continuationTasks.ProductMas());
+            Task task3 = task2.ContinueWith(t => continuationTasks.EvenNumbers());
+            task1.Start();
+            task3.Wait();
+            
         }
 
        
