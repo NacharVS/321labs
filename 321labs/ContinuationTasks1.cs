6aca21b75f5626ddf7f31b33a5bd758ba2387979
Ranil777 <x-ranil2002@mail.ru>
2021-01-21 16:20:12 +0300

    ContinuationTasks 20.01.21

---
+++ 321labs/ContinuationTasks1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _321labs
+{
+    class ContinuationTasks1
+    {
+        public static int[] array = new int[20];
+    }
+}
