59af7569b4058cd7a5ce0a2aff13d60957c986c7
Rtakale <dialogbgr@mail.ru>
2021-01-23 11:03:13 +0300

    Start CancellationTokenEx

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -13,15 +13,61 @@ namespace _321labs
 
         static void Main(string[] args)
         {
-            ContinuationTasks continuationTasks = new ContinuationTasks();
-            Task task1 = new Task(continuationTasks.RandomElevent);
-            Task task2 = task1.ContinueWith(t => continuationTasks.ProductMas());
-            Task task3 = task2.ContinueWith(t => continuationTasks.EvenNumbers());
-            task1.Start();
-            task3.Wait();
-            
-        }
+            CancellationTokenSource cancellationTokenForTask1 = new CancellationTokenSource();
+            CancellationTokenSource cancellationTokenForTask2 = new CancellationTokenSource();
+            CancellationTokenSource cancellationTokenForTask3 = new CancellationTokenSource();
+            Task task1 = new Task(() =>
+            {
+                if (cancellationTokenForTask1.Token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция по сумме не запускается");
+                    return;
+                }
 
+                Random random = new Random();
+                int result = 0;
+                for (int i = 0; ; i++)
+                {
+                    result += random.Next(1, 100);
+                    Console.WriteLine(result);
+                    Thread.Sleep(1000);
+                }
+            });
+            Task task2 = new Task(() => 
+            {
+                if (cancellationTokenForTask2.Token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция по генерации рандомных чисел не запускается");
+                    return;
+                }
+                Random random = new Random();
+                for (int i = 0; ; i++)
+                {
+                    Console.WriteLine(random.Next(1, 100));
+                }
+            });
+            Task task3 = new Task(() =>
+            {
+                if (cancellationTokenForTask3.Token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция по вычислению корня не запускается");
+                    return;
+                }
+                Random random = new Random();
+                for (int i = 0; ; i++)
+                {
+                    Console.WriteLine(Math.Sqrt(random.Next(1, 100)).ToString("0.00"));
+                }
+            });
+            task1.Start();
+            task2.Start();
+            task3.Start();
+
+            task1.Wait();
+            task2.Wait();
+            task3.Wait();
+        }
+       
        
     }
 }
