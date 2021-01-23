406696a0688de54df397bb8603b647ea6c973e2f
tigranv4 <77329623+tigranv4@users.noreply.github.com>
2021-01-23 11:08:57 +0300

    Add files via upload
    lesson 23.01.2021

---
+++ 321labs/CancellationTokenEx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _321labs
+{
+    class CancellationTokenEx
+    {
+        static int Step2(int x, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Операция прервана токеном");
+            }
+            x *= x;
+            return x;
+        }
+        static int EvenDigits(int x, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Операция прервана токеном");
+            }
+            if (x % 2 == 0)
+            {
+                Console.WriteLine($"Число {x} четное");
+            }
+            else
+            {
+                Console.WriteLine($"Число {x} нечетное");
+            }
+            return x;
+        }
+
+    }
+}
