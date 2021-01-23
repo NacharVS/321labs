28c7d1ed62c079449acd19565ebdc4db30ee7940
nectarine <name.ramil.aq@mail.ru>
2021-01-23 11:37:25 +0300

    CancellationTokenEx end

--- 321labs/CancellationTokenEx/CancellationTokenEx.cs
+++ 321labs/CancellationTokenEx/CancellationTokenEx.cs
@@ -7,7 +7,7 @@ using _321labs;
 
 namespace _321labs.CancellationTokenEx
 {
-    public static class CancellationTokenEx
+    class CancellationTokenEx
     {
         public static void CTE()
         {
@@ -31,11 +31,46 @@ namespace _321labs.CancellationTokenEx
                     Thread.Sleep(5000);
                 }
             });
+
+            Task task2 = new Task(() =>
+            {
+                int result = 1;
+                for (int i = 1; i <= number; i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Операция прервана");
+                        return;
+                    }
+
+                    result *= i;
+                    Console.WriteLine($"Факториал числа {number} равен {result}");
+                    Thread.Sleep(5000);
+                }
+            });
+
+            Task task3 = new Task(() =>
+            {
+                int result = 1;
+                for (int i = 1; i <= number; i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Операция прервана");
+                        return;
+                    }
+
+                    result *= i;
+                    Console.WriteLine($"Факториал числа {number} равен {result}");
+                    Thread.Sleep(5000);
+                }
+            });
             task1.Start();
 
-            Console.WriteLine("Введите Y для отмены операции или другой символ для ее продолжения:");
+            Console.WriteLine("Введите 'stop' для отмены операции или другой символ для ее продолжения:");
             string s = Console.ReadLine();
-            if (s == "Y")
+            if (s == "stop")
+
                 cancelTokenSource.Cancel();
 
             Console.Read();
