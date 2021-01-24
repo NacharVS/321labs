ceceb715e5f7798a38f7bda0dca720f96d7602ec
Ydin321 <andrey302230221@gmail.com>
2021-01-24 23:44:40 +0300

    5 labka 2 commit

--- 321labs/Ydin321/CancellationTokenEx/CancellationTokenEx.cs
+++ 321labs/Ydin321/CancellationTokenEx/CancellationTokenEx.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace _321labs.Ydin321.CancellationTokenEx
 {
     class CancellationTokenEx
     {
-    }
+        int[] array = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        static CancellationTokenSource token1 = new CancellationTokenSource();
+        static CancellationTokenSource token2 = new CancellationTokenSource();
+        static CancellationTokenSource token3 = new CancellationTokenSource();
+
+    
 }
+    }
+
