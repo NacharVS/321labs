70a5b7f3a9afb087b27b9a3271334918284f2c85
nectarine <name.ramil.aq@mail.ru>
2021-01-25 11:09:01 +0300

    CancellationTokenEx edit

--- 321labs/CancellationTokenEx/CancellationTokenEx.cs
+++ 321labs/CancellationTokenEx/CancellationTokenEx.cs
@@ -9,33 +9,27 @@ namespace _321labs.CancellationTokenEx
 {
     class CancellationTokenEx
     {
-        static CancellationTokenSource cancelTokenSource1 = new CancellationTokenSource();
-        static CancellationTokenSource cancelTokenSource2 = new CancellationTokenSource();
-        static CancellationTokenSource cancelTokenSource3 = new CancellationTokenSource();
-        
+        private static int[] mas = new int[10];
+        private static int a, b, c;
+        private static int z = 3;
+        private static Random r = new Random();
 
-        static int[] mas = new int[10];
-        static int a, b, c;
-        static Random r = new Random();
-
-         public static void Array()
+        private static void Array(CancellationToken token1)
         {
-
-            CancellationToken token1 = cancelTokenSource1.Token;
-            Console.WriteLine("Через 10 секунд запустится операция массива");
-            Thread.Sleep(10000);
+            Console.WriteLine("Через 5 секунд запустится операция массива");
+            Thread.Sleep(5000);
+            if (token1.IsCancellationRequested)
+                return;
             Console.Write("Нужно заполнить данный массив: ");
             foreach (int item in mas)
             {
                 Console.Write($"{item} ");
             }
             Console.WriteLine();
-            //Thread.Sleep(2000);
             for (int i = 0; i < mas.Length; i++)
             {
                 if (token1.IsCancellationRequested)
                 {
-                    Console.WriteLine("Операция массива прервана");
                     return;
                 }
 
@@ -45,45 +39,35 @@ namespace _321labs.CancellationTokenEx
                 {
                     Console.Write($"{item} ");
                 }
-                // Thread.Sleep(1000);
                 Console.WriteLine();
+                Thread.Sleep(1000);
             }
             Console.WriteLine();
             Console.WriteLine("Массив заполнен");
             Console.WriteLine();
-            //Thread.Sleep(3000);
+            z--;
         }
-        public static void Sum()
+        private static void Sum(CancellationToken token2)
         {
-            CancellationToken token2 = cancelTokenSource2.Token;
-            Console.WriteLine("Через 20 секунд запустится операция суммы");
-            Thread.Sleep(20000);
+            Console.WriteLine("Через 10 секунд запустится операция суммы");
+            if (token2.IsCancellationRequested)
+                return;
+            Thread.Sleep(10000);
             a = r.Next(1, 10);
             b = r.Next(1, 10);
             c = a + b;
-            for (int i = 0; i < c; i++)
-            {
-                if (token2.IsCancellationRequested)
-                {
-                    Console.WriteLine("Операция суммы прервана");
-                    return;
-                }
-            }
-
             Console.WriteLine($"Сумма чисел {a} и {b} равен {c}");
-            //Thread.Sleep(3000);
+            z--;
         }
-        public static void Product()
+        private static void Product(CancellationToken token3)
         {
-            CancellationToken token3 = cancelTokenSource3.Token;
-            Console.WriteLine("Через 30 секунд запустится операция произведения");
-            Thread.Sleep(30000);
+            Console.WriteLine("Через 15 секунд запустится операция произведения");
+            Thread.Sleep(15000);
             a = 1;
             for (int i = 0; i < mas.Length; i++)
             {
                 if (token3.IsCancellationRequested)
                 {
-                    Console.WriteLine("Операция произведения прервана");
                     return;
                 }
 
@@ -91,38 +75,56 @@ namespace _321labs.CancellationTokenEx
                 c = a * b;
                 Console.WriteLine($"{a} умножить на {b} равно {c}");
                 a = c;
-                //Thread.Sleep(3000);
+                z--;
             }
         }
 
-
         public static void StartTask()
         {
-            Console.WriteLine("Введите '1' для отмены операции массива");
-            Console.WriteLine("Введите '2' для отмены операции суммы");
-            Console.WriteLine("Введите '3' для отмены операци произведения");
+            Console.WriteLine("Введите '1' для отмены операции массива или любую другую для её продолжения");
+            Console.WriteLine("Введите '2' для отмены операции суммы или любую другую для её продолжения");
+            Console.WriteLine("Введите '3' для отмены операци произведения или любую другую для её продолжения");
             Console.WriteLine();
 
-            Task task1 = new Task(Array);
-            Task task2 = new Task(Sum);
-            Task task3 = new Task(Product);
+            CancellationTokenSource cancelTokenSource1 = new CancellationTokenSource();
+            CancellationToken token1 = cancelTokenSource1.Token;
+            CancellationTokenSource cancelTokenSource2 = new CancellationTokenSource();
+            CancellationToken token2 = cancelTokenSource2.Token;
+            CancellationTokenSource cancelTokenSource3 = new CancellationTokenSource();
+            CancellationToken token3 = cancelTokenSource3.Token;
+
+            Task task1 = new Task(() => Array(token1));
+            Task task2 = new Task(() => Sum(token2));
+            Task task3 = new Task(() => Product(token3));
             task1.Start();
             task2.Start();
             task3.Start();
 
-            string s = Console.ReadLine();
-            if (s == "1")
-                cancelTokenSource1.Cancel();
-            if (s == "2")
-                cancelTokenSource2.Cancel();
-            if (s == "3")
-                cancelTokenSource3.Cancel();
-            Console.Read();
-
+            for (; z > 0;)
+            {
+                string s = Console.ReadLine();
+                if (s == "1")
+                {
+                    cancelTokenSource1.Cancel();
+                    Console.WriteLine("Операция массива прервана");
+                    z--;
+                }
+                if (s == "2")
+                {
+                    cancelTokenSource2.Cancel();
+                    Console.WriteLine("Операция суммы прервана");
+                    z--;
+                }
+                if (s == "3")
+                {
+                    cancelTokenSource3.Cancel();
+                    Console.WriteLine("Операция произведения прервана");
+                    z--;
+                }
+                task1.Wait();
+                task2.Wait();
+                task3.Wait();
+            }
         }
-
-
-
-       
     }
 }
