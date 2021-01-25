70ea217dc81bc69829917ee96641678e0d9c9ee3
Domik0 <dafedot1@gmail.com>
2021-01-25 10:35:35 +0300

    finish GameCreationStart

---
+++ 321labs/Fedotov/GameCreationStart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _321labs.Fedotov
+{
+    class GameCreationStart
+    {
+        private static List<GameCreationStart> Players = new List<GameCreationStart>();
+        public string UserName { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public int FinishedMatches { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints { get; set; }
+
+        public GameCreationStart(string userName, DateTime dayOfBirth, int finishedMatches, int victories, int gamePoints)
+        {
+            UserName = userName;
+            DayOfBirth = dayOfBirth;
+            FinishedMatches = finishedMatches;
+            Victories = victories;
+            GamePoints = gamePoints;
+            Players.Add(this);
+        }
+
+        public static GameCreationStart SearchByName(string name)
+        {
+            return Players.Find(p => p.UserName == name);
+        }
+
+        public static int GameStat()
+        {
+            return Players.Sum(f => f.FinishedMatches);
+        }
+    }
+}
