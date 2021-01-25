bd2dc0c55fb49739b20dbdc3e2fa8790a892c2f5
banana <dany_021270@mail.ru>
2021-01-25 12:07:56 +0300

    12:07 ya slomal localniy repozitoriy i pochinil

---
+++ 321labs/GameCreationStart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _321labs
+{
+    class GameCreationStart
+    {
+        private string userName;
+        private DateTime dayOfBirth;
+        private int finishedMatches;
+        private int victories;
+        private int gamePoints;
+        static List<GameCreationStart> gameCreationStarts = new List<GameCreationStart>();
+
+        public DateTime DayOfBirth { get; set; }
+        public string UserName { get; set; }
+        public int FinishedMatches { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints { get; set; }
+
+
+        public GameCreationStart(string userName, int finishedMatches, int gamePoints)
+        {
+            UserName = this.userName;
+            //DayOfBirth = this.dayOfBirth;
+            FinishedMatches = this.finishedMatches;
+            GamePoints = this.gamePoints;
+            gameCreationStarts.Add(this);
+        }
+        public static string SearchByName(string UserName)
+        {
+            string s = null;
+            Parallel.ForEach(gameCreationStarts, item =>
+            {
+                if (item.UserName == UserName)
+                {
+                    s = $"имя {UserName} завершенные матчи {item.finishedMatches} поебды {item.victories}x";
+                }
+
+            }
+            );
+            return s;
+        }
+    }
+    }
