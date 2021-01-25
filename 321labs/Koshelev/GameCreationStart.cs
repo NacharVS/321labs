154d5f7d47befdf7adff1558ba1c8708b9ac25f3
Rtakale <dialogbgr@mail.ru>
2021-01-25 10:24:37 +0300

    Finish GameCreationStart

---
+++ 321labs/Koshelev/GameCreationStart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _321labs.Koshelev
+{
+    class GameCreationStart
+    {
+        static List<GameCreationStart> game = new List<GameCreationStart>();
+
+        public GameCreationStart(string userName, DateTime dayOfBirth, int finishedMatches, int victories, int gamePoints)
+        {
+            UserName = userName;
+            DayOfBirth = dayOfBirth;
+            Victories = victories;
+            GamePoints = gamePoints;
+            FinishedMatches = finishedMatches;
+
+            game.Add(this);
+        }
+
+        public string UserName { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public int FinishedMatches { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints { get; set; }
+
+        public static GameCreationStart SearchByName(string userName)
+        {
+            return game.Find(u => u.UserName == userName);
+        }
+
+        public static int GameStat()
+        {
+            return game.Sum(p => p.FinishedMatches);
+        }
+
+
+    }
+}
--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -10,80 +10,18 @@ namespace _321labs
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            CancellationTokenSource cancellationTokenForTask1 = new CancellationTokenSource();
-            CancellationTokenSource cancellationTokenForTask2 = new CancellationTokenSource();
-            CancellationTokenSource cancellationTokenForTask3 = new CancellationTokenSource();
-           
-            Task task1 = new Task(() =>
-            {
-                
-                Random random = new Random();
-                int result = 0;
-                for (int i = 0; ; i++)
-                {
-                    if (cancellationTokenForTask1.Token.IsCancellationRequested)
-                    {
-                        Console.WriteLine("Операция по сумме не запускается");
-                        return;
-                    }
-                    result += random.Next(1, 100);
-                    Console.WriteLine("Sum = " + result);
-                    Thread.Sleep(1000);
-                }
-            });
-            Task task2 = new Task(() => 
-            {
-                
-                Random random = new Random();
-                for (int i = 0; ; i++)
-                {
-                    if (cancellationTokenForTask2.Token.IsCancellationRequested)
-                    {
-                        Console.WriteLine("Операция по генерации рандомных чисел не запускается");
-                        return;
-                    }
-                    Console.WriteLine("Random = " + random.Next(1, 100));
-                    Thread.Sleep(3000);
-                }
-            });
-            Task task3 = new Task(() =>
-            {
-                
-                Random random = new Random();
-                for (int i = 0; ; i++)
-                {
-                    if (cancellationTokenForTask3.Token.IsCancellationRequested)
-                    {
-                        Console.WriteLine("Операция по вычислению корня не запускается");
-                        return;
-                    }
-                    Console.WriteLine("Sqrt = " + Math.Sqrt(random.Next(1, 100)).ToString("0.00"));
-                    Thread.Sleep(4000);
-                }
-            });
-            task1.Start();
-            task2.Start();
-            task3.Start();
-            if (Console.ReadKey().KeyChar == '1')
-            {
-                cancellationTokenForTask1.Cancel();
-            }
-            if (Console.ReadKey().KeyChar == '2')
-            {
-                cancellationTokenForTask2.Cancel();
-            }
-            if (Console.ReadKey().KeyChar == '3')
-            {
-                cancellationTokenForTask3.Cancel();
-            }
-            task1.Wait();
-            task2.Wait();
-            task3.Wait();
+            GameCreationStart user1 = new GameCreationStart("Pasha", new DateTime(2019, 02, 20), 153, 76, 350);
+            GameCreationStart user2 = new GameCreationStart("Valera", new DateTime(2018, 10, 10), 314, 203, 783);
+            GameCreationStart user3 = new GameCreationStart("Kane", new DateTime(2008, 11, 04), 2302, 1843, 4871);
+            GameCreationStart user4 = new GameCreationStart("Lil work", new DateTime(2014, 03, 20), 801, 375, 1332);
+            GameCreationStart user5 = new GameCreationStart("Pimp", new DateTime(2011, 04, 30), 1145, 743, 2105);
+            GameCreationStart user6 = new GameCreationStart("Vasa", new DateTime(2009, 01, 11), 1407, 698, 1593);
+
+            var userSearch = GameCreationStart.SearchByName("Kane");
+            Console.WriteLine(userSearch.UserName + " " + userSearch.FinishedMatches);
+            Console.WriteLine(GameCreationStart.GameStat());
         }
-       
-       
     }
 }
