5fe44945d6fa3c4a16c40d641228fd125ed09b27
Diistaf <kikopoint@mail.ru>
2021-01-26 13:17:14 +0300

    AsyncStart start

---
+++ 321labs/AsyncStart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _321labs
+{
+    class AsyncStart
+    {
+
+
+
+        public static void GenerationArr(int[] arr)
+        {
+            Random rand = new Random();
+            foreach (int item in arr)
+            {
+                arr[item] = rand.Next(1, 20);
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        public static void SumArr(int[] arr, int[] arr2,int[] arrRes)
+        {
+            foreach (int item in arrRes)
+            {
+                arrRes[item] = arr[item] + arr2[item]; 
+            }
+            foreach (int item in arrRes)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+}
