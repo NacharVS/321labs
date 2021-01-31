6286394f8831ae61c879aa3d5ec79d8cdc98066a
Ydin321 <andrey302230221@gmail.com>
2021-01-31 21:25:00 +0300

    2 commit

--- 321labs/Ydin321/GameCreationStart/GameCreationStart.cs
+++ 321labs/Ydin321/GameCreationStart/GameCreationStart.cs
@@ -6,5 +6,33 @@ namespace _321labs.Ydin321.GameCreationStart
 {
     class GameCreationStart
     {
+        private string username;
+        private int finishedmatches;
+        private int victories;
+        private int gamepoints;
+        private DateTime date1;
+
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+            private set
+            {
+                username = value;
+            }
+        }
+        public DateTime Date1
+        {
+            get
+            {
+                return date1;
+            }
+            private set
+            {
+                date1 = value;
+            }
+        }
     }
 }
