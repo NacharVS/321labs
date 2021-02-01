b811d22574bf3bd7bf0b54e844e30d9ac3bfaa8b
Domik0 <dafedot1@gmail.com>
2021-02-01 11:08:32 +0300

    update

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -13,60 +13,65 @@ namespace _321labs
             //AsyncStart threads = new AsyncStart();
             //threads.Start();
             //Console.ReadLine();
+            ArcherAddMongo();
 
+           
 
+        }
+
+        static void ArcherAddMongo()
+        {
             MongoCRUD db = new MongoCRUD("GameUnit");
             var recs = db.LoadRecords<Archer>("Archer");
 
-            //Archer a = new Archer();
+            Archer a = new Archer();
 
-            //db.InsertRecord("Archer", a);
-            //Archer a1 = new Archer()
-            //{
-            //    PositionX = 270,
-            //    PositionY = 100,
-            //    PositionZ = 40,
-            //    SpeedMove = 30,
-            //    AgilityMove = 40,
-            //    Distance = 100,
-            //    Accuracy = 40,
-            //    weapon = new Weapon("Pistol", 7, 10, 100, TimeSpan.FromSeconds(3), 800, 15)
-            //};
-            //db.InsertRecord("Archer", a1);
+            db.InsertRecord("Archer", a);
+            Archer a1 = new Archer()
+            {
+                PositionX = 270,
+                PositionY = 100,
+                PositionZ = 40,
+                SpeedMove = 30,
+                AgilityMove = 40,
+                Distance = 100,
+                Accuracy = 40,
+                weapon = new Weapon("Pistol", 7, 10, 100, TimeSpan.FromSeconds(3), 800, 15)
+            };
+            db.InsertRecord("Archer", a1);
 
-            //Archer a2 = new Archer()
-            //{
-            //    PositionX = 100,
-            //    PositionY = 300,
-            //    PositionZ = 20,
-            //    SpeedMove = 40,
-            //    AgilityMove = 26,
-            //    Distance = 250,
-            //    Accuracy = 80,
-            //    weapon = new Weapon("Sniper", 5, 6, 96, TimeSpan.FromSeconds(10), 4200, 120)
-            //};
-            //db.InsertRecord("Archer", a2);
+            Archer a2 = new Archer()
+            {
+                PositionX = 100,
+                PositionY = 300,
+                PositionZ = 20,
+                SpeedMove = 40,
+                AgilityMove = 26,
+                Distance = 250,
+                Accuracy = 80,
+                weapon = new Weapon("Sniper", 5, 6, 96, TimeSpan.FromSeconds(10), 4200, 120)
+            };
+            db.InsertRecord("Archer", a2);
 
-            //Archer a3 = new Archer()
-            //{
-            //    SpeedMove = 40,
-            //    AgilityMove = 26,
-            //    Distance = 250,
-            //    Accuracy = 80,
-            //    weapon = new Weapon("Sniper", 5, 6, 96, TimeSpan.FromSeconds(10), 4200, 120)
-            //};
-            //db.InsertRecord("Archer", a3);
+            Archer a3 = new Archer()
+            {
+                SpeedMove = 40,
+                AgilityMove = 26,
+                Distance = 250,
+                Accuracy = 80,
+                weapon = new Weapon("Sniper", 5, 6, 96, TimeSpan.FromSeconds(10), 4200, 120)
+            };
+            db.InsertRecord("Archer", a3);
 
             foreach (var rec in recs)
             {
-                Console.WriteLine($"{rec.id}: {(rec.FlagLive? "Health " + rec.Health : "Dead")}");
+                Console.WriteLine($"{rec.id}: {(rec.FlagLive ? "Health " + rec.Health : "Dead")}");
                 if (rec.weapon != null)
                 {
                     Console.WriteLine($"Weapon {rec.weapon.Name}, Bullet {rec.weapon.BulletInTheHolder}/{rec.weapon.CountHolder}, ReloadTime {rec.weapon.ReloadTime}," +
-                                  $" Damage {rec.weapon.Damage}, Durability {rec.weapon.Durability}");
+                                      $" Damage {rec.weapon.Damage}, Durability {rec.weapon.Durability}");
                 }
             }
-
         }
     }
 }
