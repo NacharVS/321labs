55fcf437927f1aed741695e3df6eb4c5ec8e4583
Diistaf <kikopoint@mail.ru>
2021-02-01 10:26:58 +0300

    Mongo start

--- 321labs/Game/Archer.cs
+++ 321labs/Game/Archer.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace _321labs.Game
 {
     class Archer : Unit, IArcher
     {
+        public int Energy { get; set; }
+        public int Dexterity { get; set; }
         public Archer(int hp, int defence, int range, int damage, int speed) : base(hp, defence, range, damage, speed)
         {
 
@@ -19,6 +24,19 @@ namespace _321labs.Game
         public void UpDamage()
         {
             this.Damage += 100;
+        }
+
+        public static async Task SaveDocs()
+        {
+            string connectionSring = "";
+            var client = new MongoClient(connectionSring);
+            var database = client.GetDatabase("321Shmelev");
+            var collection = database.GetCollection<Archer>("archer");
+            Archer archer = new Archer(100, 30, 10, 150, 15)
+            {
+
+            };
+            await collection.InsertOneAsync(archer);
         }
     }
 }
