9f7782dc45dd48ea9c58c9bb9fe55ee980db7d08
Ydin321 <andrey302230221@gmail.com>
2021-02-01 19:12:17 +0300

    2 commit

--- 321labs/Ydin321/AsyncStart/AsyncStart.cs
+++ 321labs/Ydin321/AsyncStart/AsyncStart.cs
@@ -8,6 +8,19 @@ namespace _321labs.Ydin321.AsyncStart
 {
     class AsyncStart
     {
+        static int[] args0 = new int[10];
+        static int[] args1 = new int[10];
+        static int[] args2 = new int[10];
 
+        public static void AsyncMethodsMain()
+        {
+            AsyncMain();
+            Thread.Sleep(2000);
+            foreach (var item in args2)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
-}
+    
+
