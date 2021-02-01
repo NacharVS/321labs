a537b7805ab3c7171c2f6ac0d096e82fdf86a3b1
Ydin321 <andrey302230221@gmail.com>
2021-02-01 19:10:31 +0300

    nachalo

---
+++ 321labs/Ydin321/AsyncStart/AsyncStart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _321labs.Ydin321.AsyncStart
+{
+    class AsyncStart
+    {
+
+    }
+}
