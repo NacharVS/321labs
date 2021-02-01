c7cb8e6fd659dc1a16f16312e10304c1c44bf9d6
Rtakale <dialogbgr@mail.ru>
2021-02-01 10:04:19 +0300

    Start lesson 01.02.2021

--- 321labs/Program.cs
+++ 321labs/Program.cs
@@ -5,63 +5,29 @@ using _321labs.Koshelev.Game;
 using _321labs.Koshelev.Thread;
 using _321labs.Koshelev;
 using _321labs.Game;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace _321labs
 {
     class Program
     {
-        static object locker = new object();
-        private static int[] mas1 = new int[10];
-        private static int[] mas2 = new int[10];
-        private static int[] mas3 = new int[10];
-
+        
         static void Main(string[] args)
         {
-            RandomIntNumForMas1(mas1);
-            RandomIntNumForMas2(mas2);
-            SumMas();
-            Console.ReadKey();
-        }
-        static async void RandomIntNumForMas1(int[] mas)
-        {
-            await Task.Run(() =>
-            {
-                Random random = new Random();
-                lock(locker)
-                {
-                    for (int i = 0; i < mas.Length; i++)
-                    {
-                        mas[i] = random.Next(0, 50);
-                    }
-                }
-            });
-        }
-        static async void RandomIntNumForMas2(int[] mas)
-        {
+            Warrior warrior = new Warrior 
+            { Helth = 100, 
+              Damage = 37,
+              Range = 10,
+              Speed = 46
+            };
 
-            await Task.Run(() =>
-            {
-                Random random = new Random();
-                lock(locker)
-                {
-                    for (int i = 0; i < mas.Length; i++)
-                    {
-                        mas[i] = random.Next(0, 50);
-                    }
-                }
-            });
+            string connectionString = "mongodb://localhost:27017";
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase database = client.GetDatabase("test");
+
+
         }
-        static async void SumMas()
-        {
-            await Task.Run(() =>
-            {
-                Console.Write("MasResult: ");
-                for (int i = 0; i < mas3.Length; i++)
-                {
-                    mas3[i] = mas1[i] + mas2[i];
-                    Console.Write(mas3[i] + ", ");
-                }
-            });
-        }
+      
     }
 }
